cd7ee4cb2e0f092be4eb6a486572981e63184da1
jtillil <77788257+jtillil@users.noreply.github.com>
2025-01-22 16:30:48 +0100

    Update setup_models.m

--- MAIN/setup_models.m
+++ MAIN/setup_models.m
@@ -36,6 +36,47 @@ setupModel(name, namesimple, scenario, config)
 
 %% blood coagulation
 model = struct;
+model.name = 'Wajima2009BloodCoagulation';
+model.namesimple = 'BC';
+model.scenario = 'SV';
+
+model.I = Wajima2009BloodCoagulation_indexing();
+model.I = config2I(model.I, repmat("dyn", [1, model.I.nstates]), 0);
+model.X0 = Wajima2009BloodCoagulation_initialvalues(model);
+model.par = Wajima2009BloodCoagulation_parameters(model);
+model.ode = @(t, X) Wajima2009BloodCoagulation_ode(t,X,par,model);
+
+% add relevant components
+model = create_full_model(model);
+model = model2minimal(model);
+
+% calculate indices
+if config.ir
+    [ir, contr, obs] = compute_ir_indices(model, false);
+    model.ir = ir;
+    model.contr = contr;
+    model.obs = obs;
+end
+if config.non_ir
+    model.env = compute_non_ir_indices(model, "env", false);
+    model.pneg = compute_non_ir_indices(model, "pneg", false);
+    model.cneg = compute_non_ir_indices(model, "cneg", false);
+    model.pss = compute_non_ir_indices(model, "pss", false);
+end
+if config.analyze
+    % model = compute_and_analyse_indices(model, 'compute');
+    model.threshold = 0.1;
+    model.analysis = analyse_all_indices(model);
+end
+
+% add relevant components
+model = model2minimal(model);
+
+% save model
+save(['../Core/modelfiles/model' namesimple '_' scenario '_full.mat'], 'model');
+
+%% blood coagulation - Gulati
+model = struct;
 model.name = 'Gulati2014BloodCoagulation';
 model.namesimple = 'BC_Gulati2014';
 model.scenario = 'in_vivo';
