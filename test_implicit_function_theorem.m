51747cd5983445524fcf7a80eb6b4968525d96c6
jtillil <77788257+jtillil@users.noreply.github.com>
2025-03-11 17:00:47 +0100

    implicit fct theorem

---
+++ test_implicit_function_theorem.m
@@ -0,0 +1,31 @@
+syms x1 x2 y1 y2
+F1 = x1 + 0*y1*y2 + 0*y2;  % First equation
+F2 = x2 + y1 + 5*y2; % Second equation
+
+% Define F as a vector
+F = [F1; F2];
+
+% Define variables
+X = [x1; x2];  % Independent variables
+Y = [y1; y2];  % Dependent variables
+
+% Compute Jacobian of F with respect to Y
+J = jacobian(F, Y);
+
+% Check if the Jacobian is invertible
+det_J = det(J);
+
+disp('Jacobian matrix:')
+disp(J)
+disp('Determinant of Jacobian:')
+disp(det_J)
+
+% If det(J) ≠ 0, solve for Y in terms of X
+if det_J ~= 0
+    G = solve(F, Y); % Solve for y in terms of x
+    g_x = [G.y1(1); G.y2(1)];
+    disp('Inverse function g(x):')
+    disp(g_x)
+else
+    disp('Jacobian is singular; Implicit Function Theorem does not guarantee an inverse.')
+end
