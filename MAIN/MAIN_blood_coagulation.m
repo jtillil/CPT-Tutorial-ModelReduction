c50c1c090458f4bddd7cb335b4b9fb0cfe046229
jtillil <77788257+jtillil@users.noreply.github.com>
2025-11-06 09:32:57 +0100

    remove superfluous files

--- MAIN/MAIN_blood_coagulation.m
+++ MAIN/MAIN_blood_coagulation.m
@@ -1,9 +1,8 @@
 %% Setup
 clear; clc;
 addpath(genpath("../."))
-reduced_errors = struct;
 
-%% reduce via ir-indices of 1h scenario for 40h model (using pneg, env) - derive 8-state model
+%% derive 8-state model - reduce via ir-indices of 1h scenario for 40h model (using pneg, env)
 
 model_BC_SV1 = load("modelBC_SV1_from_JKn_2024.mat").model;
 model = load("modelBC_SV40_from_JKn_2024.mat").model;
@@ -11,6 +10,14 @@ model = load("modelBC_SV40_from_JKn_2024.mat").model;
 redmodel = mor_sequential_JKn_2018(model, flip(model_BC_SV1.analysis.ir.I_sorted_max_nindex), 0.1);
 
 save("./results/modelBC_SV40_from_JKn_2024_reduced_8_state.mat", "redmodel")
+
+%% derive 13-state model - reduce via ir-indices of 40h scenario for 40h model (using pneg, env)
+
+model = load("modelBC_SV40_from_JKn_2024.mat").model;
+
+redmodel = mor_sequential_JKn_2018(model, flip(model.analysis.ir.I_sorted_max_nindex), 0.1);
+
+save("./results/modelBC_SV40_from_JKn_2024_reduced_13_state.mat", "redmodel")
 
 %% ir-indices of 8-state model
 
@@ -20,14 +27,6 @@ load("modelBC_SV40_from_JKn_2024_reduced_8_state.mat")
 [ir, contr, obs, t_ind] = compute_ir_indices_matlabfun(redmodel);
 
 save("./results/modelBC_SV40_from_JKn_2024_indices_8_state_full.mat", "ir", "contr", "obs", "t_ind")
-
-%% reduce via ir-indices of 40h scenario for 40h model (using pneg, env) - derive 13-state model
-
-model = load("modelBC_SV40_from_JKn_2024.mat").model;
-
-redmodel = mor_sequential_JKn_2018(model, flip(model.analysis.ir.I_sorted_max_nindex), 0.1);
-
-save("./results/modelBC_SV40_from_JKn_2024_reduced_13_state.mat", "redmodel")
 
 %% ir-indices of 13-state model
 
